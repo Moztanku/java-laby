4f16f7accb84c6bbfdc455a248ba04d4520dc7dc
Moztanku <mostankpl@gmail.com>
2022-04-15 20:23:42 +0200

    4.1

--- lista-4/Gui.java
+++ lista-4/Gui.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Vector;
+import src.WierszTrojkatPascala;
 
 
 
@@ -11,13 +11,36 @@ public class Gui extends Frame{
     private TextField tf;
     private Button btn;
     private Container labelContainer;
-    private Vector<Label> label;
 
     Gui(int _width,int _height){
         width = _width;
         height = _height;
         setSize(width, height);
+        setLayout(new BorderLayout());
         setVisible(true);
+
+        btn = new Button("Uruchom");
+        btn.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try{
+                    int n = Integer.parseInt(tf.getText());
+                    buttonPressed(n);
+                }
+                catch(NumberFormatException ex){
+                    labelContainer.removeAll();
+                    Label temp = new Label("Prosze wprowadzić liczbę naturalną");
+                    labelContainer.add(temp);
+                    labelContainer.revalidate();
+                }
+            }
+        });
+        btn.setFont(new Font("arial",Font.BOLD,24));
+        btn.setBackground(new Color(200,200,180));
+
+
+        tf = new TextField();
+        tf.setFont(new Font("arial",Font.PLAIN,24));
+        tf.setBackground(new Color(200,180,200));
 
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent we) {
@@ -25,22 +48,42 @@ public class Gui extends Frame{
              }
          }
         );
+        Container con = new Container();
+        labelContainer = new Container();
+        labelContainer.setBackground(new Color(190,210,210));
+        labelContainer.setLayout(new GridLayout(labelContainer.getComponentCount()+1,1));
+        labelContainer.add(new Label(""));
+        con.setLayout(new GridLayout(1,2));
+        con.add(btn);
+        con.add(tf);
+        add(con,BorderLayout.NORTH);
+        add(labelContainer,BorderLayout.CENTER);
     };
     Gui(){
         this(640,480);
     };
 
-    void addLabel(String str){
-        Label temp = new Label(str);
-        temp.setText(str);
-        temp.setFont(new Font("serif",Font.PLAIN,32));
-        label.add(temp);
-        labelContainer.setLayout(new GridLayout(1,label.size()));
-        labelContainer.add(temp);
-        add(labelContainer,BorderLayout.CENTER);
+    private void buttonPressed(int n){
+        labelContainer.setFont(new Font("arial",Font.PLAIN,6+(40-n)/2));
+        labelContainer.removeAll();
+        labelContainer.setLayout(new GridLayout(n,1));
+        for(int i=0; i<n; i++){
+            WierszTrojkatPascala wtp = new WierszTrojkatPascala(i);
+            Container con = new Container();
+            con.setLayout(new GridLayout(1,n));
+            for(int j=i; j<n; j++)
+                con.add(new Label(""));
+            for(int j=0; j<=i; j++){
+                con.add(new Label(String.valueOf(wtp.Wspolczynnik(j))));
+            }            
+            for(int j=i; j<n; j++)
+            con.add(new Label(""));
+            labelContainer.add(con);
+        }
+        labelContainer.revalidate();
     };
 
     public static void main(String[] args) {
-        new Gui();
+        Gui gui = new Gui();
     }
 }
